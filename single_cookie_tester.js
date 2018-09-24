26ec2ef2760e48d4b11a904cd545792d4f3d3b05
sethbarden <seth.barden@bounceexchange.com>
2018-09-24 11:59:32 -0400

    first commit

---
+++ single_cookie_tester.js
@@ -0,0 +1,72 @@
+var cookie = "optimizelyBuckets=%7B%7D mr_referredVisitor=0; JSESSIONID=0B71C0CC8EEF46AF0C2D29B1455201E3.prod_store05-5; anonymousOrderCookie=zTwubf+9OK3+p9/BLUdCGA==; utag_main=v_id:0165aa25c984001158a8151192a503079002307100bd0$_sn:1$_ss:0$_st:1536160532703$ses_id:1536157927816%3Bexp-session$_pn:40%3Bexp-session"
+
+function SingleCookieTester(cookie) {
+  var cookieArray = cookie.split(';').filter(Boolean);
+  var cookieObjs = makeObjs(cookieArray)
+  var cid = parseInt(localStorage.getItem('cid'))
+  var testCookie = findCookieToTest(cid, cookieObjs)
+  if (bouncex.vars.cart_qty) {
+    var cartCookie = cookieObjs.filter(function(cookie) { return cookie.id === cid })[0]
+    localStorage.removeItem('cid');
+    alert(`Your Cart Cookie name is: ${cartCookie.name}` );
+  } else if (testCookie){
+    localStorage.setItem('cid', testCookie.id)
+    replaceCookie(testCookie)
+  } else {
+    localStorage.removeItem('cid');
+    alert("You are out of cookies and the key was not found")
+  }
+
+}
+
+function makeObjs(cookieArray) {
+  return cookieArray.map(function(cookie, index) { return makeCookieObj(cookie, index) });
+}
+
+function makeCookieObj(cookie, index) {
+  var splitCookie =  cookie.trim().split('=').filter(Boolean);
+  var name = splitCookie[0]
+  var value = splitCookie[1]
+  var cookieObj = {
+    id: index + 1,
+    name: name,
+    value: value,
+  }
+  return cookieObj
+}
+
+function findCookieToTest(cid, cookieObjs) {
+  cid = cid ? cid : 0
+  return cookieObjs.filter(function(cookie) { return cookie.id === cid + 1 })[0]
+}
+
+function replaceCookie(cookieObj) {
+  var name = cookieObj.name
+  var val = cookieObj.value
+  var id = cookieObj.id
+  var domain = location.host
+  /* delete any existing cookie */
+  var domains = [
+    domain, /* with subdomain */
+    domain.replace('www', ''), /* no subdomain i.e. .forever21.com */
+    '', /* none specified, use default */
+  ];
+  for (var k = 0; k < domains.length; k++) {
+    bouncex.utils.cookies.create({
+      name: name,
+      value: '0',
+      days: -1,
+      domain: domain
+    });
+    /* write cookie value saved from previous session */
+    bouncex.utils.cookies.create({
+      name: name,
+      value: val,
+      domain: domain
+    });
+  }
+  console.log("NAME: ", name, " VALUE: ", val)
+  window.location.href = window.location.href
+}
+
+SingleCookieTester(cookie);
